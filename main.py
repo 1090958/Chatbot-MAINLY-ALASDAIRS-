7d98f16069698aef0a8344f585437ec8acaa6798
1094060 <134466720+1094060@users.noreply.github.com>
2023-05-25 12:38:35 +0800

    fixedy bixedy

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 import random
 import time
 import keyboard
-import os
-
-class biome:
-    def __init__(self, name, key):
-        self.key=key
-        self.nane=name
-
-
-
-
 def build_grid(x, y, char):
     m={}
 
@@ -25,22 +15,26 @@ def build_grid(x, y, char):
     return m
 
 class character:
-    def __init__(self, locationx, locationy, key, money):
+    def __init__(self, locationx, locationy, key, money, speed, health, damage, items):
         self.y=locationy
         self.x=locationx
         self.k=key
         self.money=money
-class village:
-    def __init__(self, locationx, locationy,  key, type):
-        self.x=locationx
-        self.y=locationy
-        self.key=key
-        self.type=type
-    def enter_village(char):
-        print("you enter a strange village")
-        ar=input("cl")
+        self.speed=speed
+        self.health=health
+        self.damage=damage
+        self.items=items
+    def checkitems(self):
+        a=len(self.items)
+        print("Items:", end="")
+        if a ==0:
+            print("nothing")
+        else:
+            for i in self.items:
+                print(i, end=" ")
+
         
-    
+player=character(0, 0, "@", 0, 10, 10, 1, ["cocian", "silly child"])
 
 def print_grid(gx, gy, character):
     a=(build_grid(gx, gy, character))
@@ -60,33 +54,66 @@ def check_grid(you, gridsize):
     while True:
         if keyboard.is_pressed("w"):
             you.y=you.y-1
-            os.system('cls||clear')
             print_grid(xl, yl, you)
         if keyboard.is_pressed("s"):
             you.y=you.y+1
-            os.system('cls||clear')
             print_grid(xl, yl, you)
         if keyboard.is_pressed("d"):
             you.x=you.x+1
-            os.system('cls||clear')
             print_grid(xl, yl, you)
-            
         if keyboard.is_pressed("a"):
             you.x=you.x-1
-            os.system('cls||clear')
             print_grid(xl, yl, you)
-        time.sleep(0)
+        time.sleep(0.1)
         if keyboard.is_pressed("m"):
             return()
-grid_size=[20, 20]
-a=input("""character key (this must be 1 letter) 
-> """)[0]
-you=character(2, 2, a, 0)
+class biome:
+    def __init__(self, x, y, key, looks, ents):
+        self.x=x
+        self.key=key
+        self.y=y
+        self.looks=looks
+        self.ents=ents
+    def enter(biome):
+        print(biome.looks)
+    def search(biome):
+        l=len(biome.ents)
+        vowels=["a", "e", "i", "o", "u"]
 
-while True:
-    if keyboard.is_pressed("m"):
-        check_grid(you, grid_size)
-        print("you left the map")
-        time.sleep(1)
+        print("you see")
+
+        for i in biome.ents:
+            if biome.ents.index(i)==(l-2):
+                if i[0] in vowels:
+                    print(f"an {i} and ")
+                else:
+                    print(f"a {i} and ")
+            elif biome.ents.index(i)==(l-1):
+                if i[0] in vowels:
+                    print(f"an {i}")
+                else:
+                    print(f"a {i}")
+            else:
+                if i[0] in vowels:
+                    print(f"an {i}, ")
+                else:
+                    print(f"a {i}, ")
+new_land=biome(3, 4, "k", "a barren wasteland, grey clouds covering the sky and the sun, the smell of rotting flesh and stagnant air seeping into your cloths and nose", ["corrupted man", "Beast", "mongrelic parasite", "amathion"])
+
+new_land.enter()
+new_land.search()
     
-    time.sleep(0.1)
+
+
+        
+
+
+
+
+
+
+
+
+
+
+

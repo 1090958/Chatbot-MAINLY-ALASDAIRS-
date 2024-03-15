9b20092525df6b000471a3744dd812321b52351c
1090958 <92854578+1090958@users.noreply.github.com>
2024-03-15 10:35:00 +0800

    Combat Beta

--- calculations.py
+++ calculations.py
@@ -1,3 +1,5 @@
+from character import character
+from math import sqrt as root
 def LCM(self, Num1, Num2):
     if Num1 >= Num2:
         Greater = Num1
@@ -10,3 +12,20 @@ def LCM(self, Num1, Num2):
             break
         Greater += 1
     return LCM
+
+def distance(point1:character|list[int,int], point2:character|list[int,int], accuracy=15):
+    
+    if type(point1) == character:
+        point1=point1.coordinates
+    if type(point2) == character:
+        point2=point2.coordinates
+    
+    return round(root((point1[0]-point2[0])**2 + (point1[1]-point2[1])**2),accuracy)
+
+
+p1=character(10,5,15)
+p2=character(10,5,15)
+p2.coordinates = [8,8]
+
+
+print(distance(p1,p2))

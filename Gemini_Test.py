88a0808a2185498261f73c6d91756c136037ccbd
1090958 <92854578+1090958@users.noreply.github.com>
2024-03-17 16:29:50 +0800

    Bot Framework Composer
    But at what cost???

--- Gemini_Test.py
+++ Gemini_Test.py
@@ -16,7 +16,7 @@ headers = {
 params = {
     'key': os.getenv('API_KEY', 'AIzaSyCqQIqwbVwFaEBJYcDKmhi-uY-Shk8e6oE'),
 }
-def querie(input):
+def query(input):
     json_data = {
         'contents': [
             {
@@ -27,6 +27,21 @@ def querie(input):
                 ],
             },
         ],
+        'safetySettings': [
+            {
+                'category': 'HARM_CATEGORY_DANGEROUS_CONTENT',
+                'threshold': 'BLOCK_ONLY_HIGH',
+            },
+        ],
+        'generationConfig': {
+            'stopSequences': [
+                'Title',
+            ],
+            'temperature': 1.0,
+            'maxOutputTokens': 100,
+            'topP': 0.6,
+            'topK': 2,
+        },
     }
     response = None
     try:
@@ -39,10 +54,10 @@ def querie(input):
         )
         response.raise_for_status()
     except HTTPError as http_err:
-        # a 503 error is parsed when the server is overloaded, because we can't verify html there are some minor errors
+        # a 503 error is parsed when the server is overloaded, because we can't verify curls, there are some minor errors
         if "503" in str(http_err):
             time.sleep(2)
-            return(querie(input))
+            return(query(input))
         print(f"{http_err}")
         quit()
     except Exception as err:
@@ -56,5 +71,26 @@ def querie(input):
         ],
         "role":"""
     return(response.text[response.text.find('text')+8:response.text.find(end)]) # type: ignore
+
+intents = ['attack', 'heal', 'intimidate', 'charm', 'none']
+def get_Intent(message):
+    for i in range(10):
+        response = query(f'return the top 3 possibilities of the user\'s intents from this message, \'{message}\', it may be from one of the following intents:\n -  {'\n -  '.join(intents)}\n Only reply using the top 3 of the intents, in order of descending weight using lowercase letters, with no punctuation or other words, only a space separating each one and the weights of each one')
+        count = 0
+        print(response)
+        for intent in intents:
+            if intent in response:
+                
+                count += 1
+                
+        if count == 3 and len(response.split()) == 6:
+            break
+        else:
+            print(i)
+    output = {}
+    for i in range(1,4):
+        pass
+        #print(i)
+    return response
 if __name__ == "__main__":
-    print(querie(input()))
+    print(query(input()))
